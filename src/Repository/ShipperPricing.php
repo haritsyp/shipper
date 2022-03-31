ac972f95c08df5662962d072c9cae801858b2eeb
Harits Yulianta Pratama <harits.pratama@metrodata.co.id>
2022-03-31 13:31:21 +0700

    shipper api add pricing location cordinate

--- src/Repository/ShipperPricing.php
+++ src/Repository/ShipperPricing.php
@@ -39,4 +39,57 @@ class ShipperPricing extends Shipper
             'for_order' => $for_order
         ));
     }
+
+    /**
+     * @param array $origin
+     * @param array $destination
+     * @param int $length
+     * @param int $width
+     * @param int $height
+     * @param float $weight
+     * @param float $price
+     * @param bool $for_order
+     * @return mixed
+     */
+    public function getPricingDomestic(array $origin, array $destination, int $length, int $width, int $height, float $weight, float $price, $for_order = true)
+    {
+        return $this->post('v3/pricing/domestic', array(
+            'destination' => $destination,
+            'origin' => $origin,
+            'length' => $length,
+            'width' => $width,
+            'height' => $height,
+            'weight' => $weight,
+            'limit' => 100,
+            'item_value' => $price,
+            'for_order' => $for_order
+        ));
+    }
+
+    /**
+     * @param string $rate_type
+     * @param array $origin
+     * @param array $destination
+     * @param int $length
+     * @param int $width
+     * @param int $height
+     * @param float $weight
+     * @param float $price
+     * @param bool $for_order
+     * @return mixed
+     */
+    public function getPricingDomesticByRateType(string $rate_type, array $origin, array $destination, int $length, int $width, int $height, float $weight, float $price, $for_order = true)
+    {
+        return $this->post('v3/pricing/domestic/' . $rate_type, array(
+            'destination' => $destination,
+            'origin' => $origin,
+            'length' => $length,
+            'width' => $width,
+            'height' => $height,
+            'weight' => $weight,
+            'limit' => 100,
+            'item_value' => $price,
+            'for_order' => $for_order
+        ));
+    }
 }
